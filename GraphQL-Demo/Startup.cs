c5d0a05c370520eece9cb1a13be6715a27cbf8aa
yaozu.lei <yaozu.lei>
2021-08-17 21:44:38 +0800

    init code

--- GraphQL-Demo/Startup.cs
+++ GraphQL-Demo/Startup.cs
@@ -18,7 +18,6 @@ namespace GraphQL_Demo
         {
         }
 
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
